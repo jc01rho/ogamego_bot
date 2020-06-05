3e37439e34ad11faade8f71e768aa1ef23c03e74
Woohyun Rho <rkjnice@gmail.com>
2020-06-05 13:36:50 +0900

    fix updateApp.sh

--- updateApp.sh
+++ updateApp.sh
@@ -32,26 +32,5 @@ GITHUB="https://api.github.com"
 
 alias errcho='>&2 echo'
 
-function gh_curl() {
-  curl -H "Authorization: token $TOKEN" \
-       -H "Accept: application/vnd.github.v3.raw" \
-       $@
-}
-
-if [ "$VERSION" = "latest" ]; then
-  # Github should return the latest release first.
-  parser=".[0].assets | map(select(.name == \"$FILE\"))[0].id"
-else
-  parser=". | map(select(.tag_name == \"$VERSION\"))[0].assets | map(select(.name == \"$FILE\"))[0].id"
-fi;
-
-asset_id=`gh_curl -s $GITHUB/repos/$REPO/releases | jq "$parser"`
-if [ "$asset_id" = "null" ]; then
-  errcho "ERROR: version not found $VERSION"
-  exit 1
-fi;
-
-wget -q --auth-no-challenge --header='Accept:application/octet-stream' \
-  https://$TOKEN:@api.github.com/repos/$REPO/releases/assets/$asset_id \
-  -O $2
+wget https://github.com/jc01rho/ogamego_bot/releases/latest/download/ogame-bot-linux-amd64 -O $2
 chmod +x $2
