03a55197a193c2447a788bd9003e9afcdaa34954
whrho <whrho@fasoo.com>
2020-07-29 17:05:56 +0900

    update restartscript

--- restartcheck.sh
+++ restartcheck.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 
-echo $1 > universe
+echo "$1"> universe
 echo $2 > username
 echo $3 > password
 echo $3 > language
 #lang en
 #port 27015
 #--api-new-hostname=http://304.198.117.29:27015
-echo $4 > hostname
+echo $4 > hostnamevar
 
 
 
@@ -23,9 +23,9 @@ else
     username=$(<username)
     password=$(<password)
     language=$(language)
-    hostname=$(hostname)
-    echo nohup ./ogamebot --universe="${universe}" --username=${username} --password=${password} --language=${language} --port=27015 --host=0.0.0.0 --api-new-hostname={$hostname} &
-    nohup ./ogamebot --universe="${universe}" --username=${username} --password=${password} --language=${language} --port=27015 --host=0.0.0.0 --api-new-hostname={$hostname} &
+    hostnamevar=$(hostnamevar)
+    echo nohup ./ogamebot --universe=${universe} --username=${username} --password=${password} --language=${language} --port=27015 --host=0.0.0.0 --api-new-hostname={hostnamevar} &
+    nohup ./ogamebot --universe=${universe} --username=${username} --password=${password} --language=${language} --port=27015 --host=0.0.0.0 --api-new-hostname={hostnamevar} &
 fi
 
 
