9b79c699b490868fef9f3ad0b0c7afbf17cabbb9
jc01rho <rkjnice@gmail.com>
2020-08-17 11:45:54 +0900

    update logic

--- Global/InstanceInitializer.go
+++ Global/InstanceInitializer.go
@@ -18,19 +18,21 @@ func InitEssentials(bot *ogame.OGame, c *cli.Context) {
 	OGameBotRoutine.OGameBotGlobal.Ogamebot = bot
 
 	OGameBotRoutine.OGameBotGlobal.IsExpeditionMode = c.Bool("expedtiion-mode")
-	stringCoords := strings.Split(c.String("main-coords"), ":")
+	if c.String("main-coords") != "" {
+		stringCoords := strings.Split(c.String("main-coords"), ":")
 
-	tempMainCoords := new(ogame.Coordinate)
-	tempMainCoords.Galaxy, _ = strconv.ParseInt(stringCoords[0], 10, 64)
-	tempMainCoords.System, _ = strconv.ParseInt((stringCoords[1]), 10, 64)
-	tempMainCoords.Position, _ = strconv.ParseInt((stringCoords[2]), 10, 64)
-	typeTmp, _ := (strconv.ParseInt((stringCoords[3]), 10, 64))
-	tempMainCoords.Type = ogame.CelestialType(typeTmp)
-	OGameBotRoutine.OGameBotGlobal.MainCoords = tempMainCoords
+		tempMainCoords := new(ogame.Coordinate)
+		tempMainCoords.Galaxy, _ = strconv.ParseInt(stringCoords[0], 10, 64)
+		tempMainCoords.System, _ = strconv.ParseInt((stringCoords[1]), 10, 64)
+		tempMainCoords.Position, _ = strconv.ParseInt((stringCoords[2]), 10, 64)
+		typeTmp, _ := (strconv.ParseInt((stringCoords[3]), 10, 64))
+		tempMainCoords.Type = ogame.CelestialType(typeTmp)
+		OGameBotRoutine.OGameBotGlobal.MainCoords = tempMainCoords
 
-	OGameBotRoutine.OGameBotGlobal.Class = OGameBotRoutine.OGameBotGlobal.Ogamebot.CharacterClass()
-	OGameBotRoutine.OGameBotGlobal.DetermineMainPlanet()
-	OGameBotRoutine.OGameBotGlobal.BuildRessSkipList = hashset.New()
+		OGameBotRoutine.OGameBotGlobal.Class = OGameBotRoutine.OGameBotGlobal.Ogamebot.CharacterClass()
+		OGameBotRoutine.OGameBotGlobal.DetermineMainPlanet()
+		OGameBotRoutine.OGameBotGlobal.BuildRessSkipList = hashset.New()
+	}
 
 	Queue.InitQueue()
 	//Bot.InitBot()
