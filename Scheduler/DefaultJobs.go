d7271b2d9b9ef899179df5176650a7e6f0053549
whrho <whrho@sparrowfasoo.com>
2021-06-18 13:48:08 +0900

    fix base ogamed

--- Scheduler/DefaultJobs.go
+++ Scheduler/DefaultJobs.go
@@ -19,7 +19,7 @@ func DefaultJobs() {
 
 	Scheduler.Every(1).Day().At("08:00").Do(Queue.JobQueue.Set, Queue.CriticalPriority, OGameBotRoutine.RestartLogic)
 
-	Scheduler.Every(300).Minutes().At("08:04").Do(Queue.JobQueue.Set, Queue.DefaultPriority, OGameBotRoutine.OGameBotGlobal.BuildDefs)
+	Scheduler.Every(20).Minutes().At("08:04").Do(Queue.JobQueue.Set, Queue.DefaultPriority, OGameBotRoutine.OGameBotGlobal.BuildDefs)
 	Scheduler.Every(25).Minutes().At("08:03").Do(Queue.JobQueue.Set, Queue.DefaultPriority, OGameBotRoutine.OGameBotGlobal.MaintainLCCountStep, int64(-1))
 	Scheduler.Every(11).Hours().At("08:06").Do(Queue.JobQueue.Set, Queue.DefaultPriority, OGameBotRoutine.OGameBotGlobal.CollectRessRoutine)
 	Scheduler.Every(2).Hours().At("11:01").Do(Queue.JobQueue.Set, Queue.DefaultPriority, OGameBotRoutine.OGameBotGlobal.BuildNextRess)
